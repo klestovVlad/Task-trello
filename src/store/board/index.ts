339148dd52bb1da066cfe13eeb661d03ca8366c4
klestovVlad <klestov.vlad@gmail.com>
2021-08-10 16:56:36 +0600

    delete types

--- src/store/board/index.ts
+++ src/store/board/index.ts
@@ -1,7 +1,6 @@
 import reducer from "./slice";
 
 export { boardAction } from "./slice";
-export { default as boardTypes } from "./types";
 export { selectStoreData } from "./selectors";
 
 export default reducer;
